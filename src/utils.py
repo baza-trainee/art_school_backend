5d5210952be4443f25e3e6debd2750c475705a86
smile4alice <smile.to.alice@gmail.com>
2023-11-20 11:48:50 +0200

    fix: trying use old version lifespan for versel

--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,26 @@
+# non-business logic functions, e.g. response normalization, data enrichment, etc.
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from sqlalchemy import select
+from src.contacts.models import Contacts
 
-from src.auth.utils import create_user
-from src.config import ADMIN_PASSWORD, ADMIN_USERNAME
-from src.contacts.utils import create_contacts
 from src.database import get_async_session
 
 
+@asynccontextmanager
 async def lifespan(app: FastAPI):
     print("lifespan start")
-    async for s in get_async_session():
+    session = get_async_session()
+    async for s in session:
         async with s.begin():
-            # await create_user(email=ADMIN_USERNAME, password=ADMIN_PASSWORD)
-            await create_contacts(
-                address="вул.Бульварно-Кудрявська, 2", phone="+38(097)290-79-40"
-            )
+            query = select(Contacts)
+            result = await s.execute(query)
+            contacts = result.scalars().first()
+            if not contacts:
+                contacts = Contacts(
+                    address="вул.Бульварно-Кудрявська, 2", phone="+38(097)290-79-40"
+                )
+                s.add(contacts)
+                await s.commit()
     yield
     print("lifespan end")
--- vercel.py
+++ vercel.py
@@ -1,21 +1,23 @@
+from sqlalchemy import select
+from src.contacts.models import Contacts
 
 from src.database import get_async_session
-from src.auth.utils import create_user
 
-from src.auth.utils import create_user
-from src.config import ADMIN_PASSWORD, ADMIN_USERNAME
-from src.contacts.utils import create_contacts
-from src.database import get_async_session
 
 async def customlifespan():
     print("lifespan start")
     session = get_async_session()
     async for s in session:
         async with s.begin():
-            # await create_user(email=ADMIN_USERNAME, password=ADMIN_PASSWORD)
-            await create_contacts(
-                address="вул.Бульварно-Кудрявська, 2", phone="+38(097)290-79-40"
-            )
+            query = select(Contacts)
+            result = await s.execute(query)
+            contacts = result.scalars().first()
+            if not contacts:
+                contacts = Contacts(
+                    address="вул.Бульварно-Кудрявська, 2", phone="+38(097)290-79-40"
+                )
+                s.add(contacts)
+                await s.commit()
     print("lifespan end")
 
 
