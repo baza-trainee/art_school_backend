a3c8f865113b8dea0b88020e5fd83a48cedde8d4
smile4alice <smile.to.alice@gmail.com>
2023-11-27 12:10:35 +0200

    fix: delete admin panel

--- admin/posters.py
+++
@@ -1,7 +0,0 @@
-from sqladmin import ModelView
-
-from src.posters.models import Poster
-
-
-class PosterView(ModelView, model=Poster):
-    column_list = [Poster.id, Poster.title, Poster.text, Poster.photo]
