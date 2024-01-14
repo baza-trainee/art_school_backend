2869635d8f66946ae2c2f4dcd60ba3f6c11a1ac1
atelay <deadroll95@gmail.com>
2024-01-14 21:07:53 +0200

    fix contacts swagger

--- src/contacts/models.py
+++ src/contacts/models.py
@@ -7,11 +7,11 @@ class Contacts(Base):
     __tablename__ = "contacts"
 
     id = Column(Integer, primary_key=True)
-    map_url = Column(String)
-    address = Column(String)
+    map_url = Column(String(length=1000))
+    address = Column(String(length=300))
     phone = Column(String(length=50))
     email = Column(String(length=50))
-    facebook_url = Column(String)
-    youtube_url = Column(String)
-    statement_for_admission = Column(String)
-    official_info = Column(String)
+    facebook_url = Column(String(length=1000))
+    youtube_url = Column(String(length=1000))
+    statement_for_admission = Column(String(length=1000))
+    official_info = Column(String(length=1000))
