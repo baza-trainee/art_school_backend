21d82c218b9406177cd48fbb9ee795ce63cfb7ca
atelay <deadroll95@gmail.com>
2024-01-15 22:32:19 +0200

    change validation for school administrations

--- src/administrations/models.py
+++ src/administrations/models.py
@@ -7,6 +7,6 @@ class SchoolAdministration(Base):
     __tablename__ = "school_administrations"
 
     id = Column(Integer, primary_key=True)
-    full_name = Column(String(length=150))
-    position = Column(String(length=2000))
+    full_name = Column(String(length=60))
+    position = Column(String(length=120))
     photo = Column(String(length=300))
