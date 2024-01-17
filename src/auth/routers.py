d9f046939fcf378df51a103214d340504362adbc
atelay <deadroll95@gmail.com>
2024-01-17 12:03:33 +0200

    change status code to 422 in change password, change sending mail time

--- src/auth/routers.py
+++ src/auth/routers.py
@@ -1,8 +1,21 @@
-from fastapi import APIRouter, Depends, Form, HTTPException
-from fastapi_users import InvalidPasswordException
-from fastapi_users.password import PasswordHelper
-from sqlalchemy.ext.asyncio import AsyncSession
+from pydantic import EmailStr
 from sqlalchemy import update
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import (
+    APIRouter,
+    BackgroundTasks,
+    Body,
+    Depends,
+    Form,
+    HTTPException,
+    Request,
+    status,
+)
+from fastapi_users import InvalidPasswordException, exceptions, models
+from fastapi_users.password import PasswordHelper
+from fastapi_users.manager import BaseUserManager
+from fastapi_users.router.common import ErrorCode
+from fastapi_users.router.reset import RESET_PASSWORD_RESPONSES
 
 from src.auth.auth_config import CURRENT_SUPERUSER, fastapi_users, auth_backend
 from src.database.database import get_async_session
@@ -50,7 +63,63 @@ async def change_password(
     return {"detail": PASSWORD_CHANGE_SUCCESS}
 
 
+@auth_router.post(
+    "/forgot-password",
+    status_code=status.HTTP_202_ACCEPTED,
+    name="reset:forgot_password",
+)
+async def forgot_password(
+    request: Request,
+    background_tasks: BackgroundTasks,
+    email: EmailStr = Body(..., embed=True),
+    user_manager: BaseUserManager[models.UP, models.ID] = Depends(get_user_manager),
+):
+    try:
+        user = await user_manager.get_by_email(email)
+    except exceptions.UserNotExists:
+        return None
+
+    try:
+        await user_manager.forgot_password(user, background_tasks, request)
+    except exceptions.UserInactive:
+        pass
+
+    return None
+
+
+@auth_router.post(
+    "/reset-password",
+    name="reset:reset_password",
+    responses=RESET_PASSWORD_RESPONSES,
+)
+async def reset_password(
+    request: Request,
+    token: str = Body(...),
+    password: str = Body(...),
+    user_manager: BaseUserManager[models.UP, models.ID] = Depends(get_user_manager),
+):
+    try:
+        await user_manager.reset_password(token, password, request)
+    except (
+        exceptions.InvalidResetPasswordToken,
+        exceptions.UserNotExists,
+        exceptions.UserInactive,
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=ErrorCode.RESET_PASSWORD_BAD_TOKEN,
+        )
+    except exceptions.InvalidPasswordException as e:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail={
+                "code": ErrorCode.RESET_PASSWORD_INVALID_PASSWORD,
+                "reason": e.reason,
+            },
+        )
+
+
 auth_router.include_router(
     fastapi_users.get_auth_router(auth_backend, requires_verification=True)
 )
-auth_router.include_router(fastapi_users.get_reset_password_router())
+# auth_router.include_router(fastapi_users.get_reset_password_router())
